ed33bf736f4a8e6b8de8a9e023a42eceab6fb3ad
Nisanth-2025 <nisanth252025@gmail.com>
2025-02-25 16:40:47 +0530

    First Commit

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        int num1, num2, sum;
+        cout << "Enter the 1st Number :";
+        cin >> num1;
+        cout << "Enter the 2nd Number :";
+        cin >> num2;
+        sum = num1 + num2;
+        cout << "The Sum is" << sum;
+        return 0;
+}
