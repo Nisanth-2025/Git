8b609757b217030aaa3d033a7bfaa2454a55a823
Nisanth-2025 <nisanth252025@gmail.com>
2025-02-25 16:42:54 +0530

    Second Commit

--- Hello.cpp
+++ Hello.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
-        int num1, num2, sum;
+        int num1, num2, num3, sum;
         cout << "Enter the 1st Number :";
         cin >> num1;
         cout << "Enter the 2nd Number :";
         cin >> num2;
-        sum = num1 + num2;
+	cout << "Enter the 3rd Number :";
+	cin >> num3;
+        sum = num1 + num2 + num3;
         cout << "The Sum is" << sum;
         return 0;
 }
