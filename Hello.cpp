af423727c1d2a773401d2606a701daed3bf3f8fe
Nisanth-2025 <nisanth252025@gmail.com>
2025-02-25 16:45:30 +0530

    Third Commit

--- Hello.cpp
+++ Hello.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-        int num1, num2, num3, sum;
-        cout << "Enter the 1st Number :";
-        cin >> num1;
-        cout << "Enter the 2nd Number :";
-        cin >> num2;
-	cout << "Enter the 3rd Number :";
-	cin >> num3;
-        sum = num1 + num2 + num3;
-        cout << "The Sum is" << sum;
-        return 0;
+
+int Calc(int a, int b, int c) {
+    return a + b + c;
+}
+
+int main() {
+    int num1, num2, num3, sum;
+    
+    cout << "Enter the 1st Number: ";
+    cin >> num1;
+    cout << "Enter the 2nd Number: ";
+    cin >> num2;
+    cout << "Enter the 3rd Number: ";
+    cin >> num3;
+    
+    sum = Calc(num1, num2, num3);
+    cout << "The Sum is: " << sum << endl;
+    
+    return 0;
 }
